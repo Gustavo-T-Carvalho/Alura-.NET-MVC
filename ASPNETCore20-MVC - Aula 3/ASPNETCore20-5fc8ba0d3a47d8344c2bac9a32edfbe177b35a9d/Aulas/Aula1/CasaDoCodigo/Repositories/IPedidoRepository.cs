5c2a1de159f1a63441bc716f7e71bf0216dab273
Gustavo <carvalho.t.gustavo@gmail.com>
2020-07-07 14:05:29 -0300

    Projeto Alura MVC

---
+++ ASPNETCore20-MVC - Aula 3/ASPNETCore20-5fc8ba0d3a47d8344c2bac9a32edfbe177b35a9d/Aulas/Aula1/CasaDoCodigo/Repositories/IPedidoRepository.cs
@@ -0,0 +1,15 @@
+﻿using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IPedidoRepository
+    {
+        Pedido GetPedido();
+        UpdateQuantidadeResponse UpdateQuantidade(ItemPedido itemPedido);
+        void AddItem(string codigo);
+        Pedido UpdateCadastro(Cadastro cadastro);
+    }
+}
+
+
+
---
+++ ASPNETCore20-MVC - Aula 4/ASPNETCore20-5fc8ba0d3a47d8344c2bac9a32edfbe177b35a9d/Aulas/Aula1/CasaDoCodigo/Repositories/IPedidoRepository.cs
@@ -0,0 +1,13 @@
+﻿using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IPedidoRepository
+    {
+        Pedido GetPedido();
+        void AddItem(string codigo);
+    }
+}
+
+
+
